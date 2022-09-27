f3705feda77cef6179231c82ce8f26082278f8c7
Brian King <bnk5532@gmail.com>
2022-09-27 18:13:14 -0400

    find all, one and create methods added to controller

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,37 @@
+const { Pizza } = require("../models");
+
+const pizzaController = {
+  getAllPizza(req, res) {
+    Pizza.find({})
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  getPizzaById({ params }, res) {
+    Pizza.findOne({ _id: params.id })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this ID!" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  createPizza ({ body }, res) {
+    Pizza.create(body)
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => res.status(400).json(err))
+  },
+};
+
+
+
+module.exports = pizzaController;
