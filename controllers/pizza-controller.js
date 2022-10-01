faa004e8f07d00939e2b3bfeedb757b978526478
Brian King <bnk5532@gmail.com>
2022-09-30 20:03:11 -0400

    virtuals and date formatting added to frontend js.

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -3,7 +3,13 @@ const { Pizza } = require("../models");
 const pizzaController = {
   getAllPizza(req, res) {
     Pizza.find({})
-      .then((dbPizzaData) => res.json(dbPizzaData))
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbPizzaData => res.json(dbPizzaData))
       .catch((err) => {
         console.log(err);
         res.status(400).json(err);
@@ -12,7 +18,12 @@ const pizzaController = {
 
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
-      .then((dbPizzaData) => {
+    .populate({
+      path: 'comments',
+      select: '-__v'
+    })
+    .select('-__v')
+    .then((dbPizzaData) => {
         if (!dbPizzaData) {
           res.status(404).json({ message: "No pizza found with this ID!" });
           return;
